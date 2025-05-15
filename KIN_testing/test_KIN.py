5e11a4ca93ca166a9bfc40f4a9ade846aafcadf5
BerendB382 <berend@nieuwnet.nl>
2025-05-15 11:34:41 +0200

    deleted cache and moved files

---
+++ KIN_testing/test_KIN.py
@@ -0,0 +1,5 @@
+import numpy as np 
+
+# This file tests the KIN for a given system. It should create a system according to the create_test_system file,
+# and take in the parameters in the job_params file. Maybe have a slurm mode? Or just allow one repetition?
+# Also point users to the validation file, and maybe streamline that. 
